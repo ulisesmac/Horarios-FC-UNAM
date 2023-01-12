f5043acfcf6645c840d63d3245ac885ff8cc567d
Ulises M <ulises.ssb@hotmail.com>
2023-01-11 18:26:48 -0600

    Create project

---
+++ src/main/horarios_fc/app.cljs
@@ -0,0 +1,31 @@
+(ns horarios-fc.app
+  (:require
+   ["react-native" :as rn]
+   [reagent.core :as r]
+   [shadow.react-native :refer (render-root)]))
+
+(def styles
+  ^js (-> {:container
+           {:flex            1
+            :backgroundColor "#fff"
+            :alignItems      "center"
+            :justifyContent  "center"}
+           :title
+           {:fontWeight "bold"
+            :fontSize   24
+            :color      "blue"}}
+          (clj->js)
+          (rn/StyleSheet.create)))
+
+(defn root []
+  [:> rn/View {:style (.-container styles)}
+   [:> rn/Text {:style (.-title styles)} "Hello!"]
+   ])
+
+(defn start
+  {:dev/after-load true}
+  []
+  (render-root "HorariosFCUNAM" (r/as-element [root])))
+
+(defn init []
+  (start))
