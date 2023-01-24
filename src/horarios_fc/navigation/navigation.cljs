61734b6f5caf3554caecce534163b952cda25c54
Ulises M <ulises.ssb@hotmail.com>
2023-01-24 16:01:43 -0600

    Add temp nav bar

--- src/horarios_fc/navigation/navigation.cljs
+++ src/horarios_fc/navigation/navigation.cljs
@@ -1,27 +1,25 @@
 (ns ^:dev/always horarios-fc.navigation.navigation
   (:require
+   [horarios-fc.colors :refer [theme]]
    [horarios-fc.navigation.screens :as screens]
    [horarios-fc.navigation.utils :as nav-utils]
    [re-frame.core :as rf]
-   [react-native :as rn]
    [react-navigation.bottom-tabs :refer [create-bottom-tab-navigator]]
-   [react-navigation.native :refer [create-navigation-container-ref navigation-container]]
-   [reagent.core :as r]))
+   [react-navigation.native :refer [create-navigation-container-ref navigation-container]]))
 
 (defonce bottom-tabs (create-bottom-tab-navigator))
-(defn tmp-tab-icon [focused color size]
-  (r/as-element [rn/view {:style {:background-color color}}
-                 [rn/text "i"]]))
 
-(defn  app-navigator []
+(defn app-navigator []
   (let [{tab-screen :tab/screen, tab-navigator :tab/navigator} bottom-tabs]
     [navigation-container {:ref             nav-utils/navigation-ref
                            :initial-state   @(rf/subscribe [:navigation-state])
                            :on-state-change #(rf/dispatch-sync
                                               [:store-navigation-&-state %])}
      [tab-navigator {:screen-options {:header-shown              false
-                                      :tab-bar-active-tint-color "green"
-                                      :tab-bar-icon              tmp-tab-icon}}
+                                      :tab-bar-active-tint-color (theme :primary-600)
+                                      :tab-bar-label-style       {:font-size     12
+                                                                  :margin-bottom 4
+                                                                  :font-weight   "600"}}}
       (map (fn [{:keys [props component] :as _tab-screen}]
              ^{:key (str (:name props))}
              [tab-screen props component])
--- src/horarios_fc/navigation/screens.cljs
+++ src/horarios_fc/navigation/screens.cljs
@@ -4,6 +4,7 @@
    [horarios-fc.screens.pick-major.views :as pick-major]
    [horarios-fc.screens.pick-plan.views :as pick-plan]
    [horarios-fc.screens.pick-subject.views :as pick-subject]
+   [react-native :as rn]
    [react-navigation.native-stack :refer [create-native-stack-navigator]]
    [reagent.core :as r]))
 
@@ -12,7 +13,8 @@
   [nav-utils/create-stack-navigator
    {:native-stack-navigator schedules-stack
     :screens                [{:component pick-major/screen
-                              :props     {:name :schedule-major}}
+                              :props     {:name    :schedule-major
+                                          :options {:title "Consulta de horarios"}}}
                              {:component pick-plan/screen
                               :props     {:name :schedule-plan}}
                              {:component pick-subject/screen
@@ -20,10 +22,29 @@
                                           :options {:header-transparent true
                                                     :header-title       ""}}}]}])
 
+
+(defn browse-schedules-icon [js-params]
+  (let [{:keys [focused color size]} (js->clj js-params :keywordize-keys true)]
+    (r/as-element
+     [rn/view
+      [rn/text {:style (merge {:font-size 24}
+                              (when focused {:color color}))}
+       "🧭"]])))
+
+(defn my-schedule-icon [js-params]
+  (let [{:keys [focused color size]} (js->clj js-params :keywordize-keys true)]
+    (r/as-element
+     [rn/view
+      [rn/text {:style (merge {:font-size 24}
+                              (when focused {:color color}))}
+       "🕰"]])))
+
 (defn tab-screens []
-  [{:props     {:name      :schedule-tab
-                :show-name "Horarios"}
+  [{:props     {:name    :schedule-tab
+                :options {:title        "Navegar horarios"
+                          :tab-bar-icon browse-schedules-icon}}
     :component #(r/as-element [schedules-stack-screen])}
-   {:props     {:name      :my-schedule
-                :show-name "Mi horario"}
+   {:props     {:name    :my-schedule
+                :options {:title "Mi horario"
+                          :tab-bar-icon my-schedule-icon}}
     :component pick-major/screen}])
